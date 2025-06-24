1645df27593643d70827fa90668a9772ee12013c
JatinK07 <153960300+JatinK07@users.noreply.github.com>
2025-06-25 00:17:41 +0530

    Update player.py

--- models/player.py
+++ models/player.py
@@ -1,1 +1,18 @@
-...
+class Player:
+    def __init__(self, player_id, name, city, rating=1000):
+        self.id = player_id
+        self.name = name
+        self.city = city
+        self.rating = rating
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "city": self.city,
+            "rating": self.rating
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data["id"], data["name"], data["city"], data["rating"])
